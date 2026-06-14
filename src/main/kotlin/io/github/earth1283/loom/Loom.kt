39b387daa5c700ca785634774085710511689c2d
Earth1283 <142399154+Earth1283@users.noreply.github.com>
2026-06-14 14:19:06 +0800

    initial commit

---
+++ src/main/kotlin/io/github/earth1283/loom/Loom.kt
@@ -0,0 +1,14 @@
+package io.github.earth1283.loom
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Loom : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
